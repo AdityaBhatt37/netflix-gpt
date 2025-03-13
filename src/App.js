8fd6cd17173b894437ecfd82e4a78e4621d3bf6e
Aditya Bhatt <bhattadi60@gmail.com>
2025-03-14 01:10:10 +0530

    netFlix-GPT React Project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+        <h1 className="text-3xl bg-blue-600">Namaste React</h1>
+    </div>
+  );
+}
+
+export default App;
